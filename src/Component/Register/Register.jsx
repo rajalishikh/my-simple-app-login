ffc1aff12867ec33170248f9849f1543e22d24ed
rajalishikhe <nahidujjamanraj@gmail.com>
2025-09-25 10:03:32 +0600

    I solve one error and reacive the user information

--- src/Component/Register/Register.jsx
+++ src/Component/Register/Register.jsx
@@ -56,7 +56,7 @@ const Register = () => {
     name="password"
     required
     placeholder="Password"
-    minlength="8"
+    minLength="8"
     pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
     title="Must be more than 8 characters, including number, lowercase letter, uppercase letter"
   />
